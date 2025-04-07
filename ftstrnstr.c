9c15eb8d50f9d236d448f490aff0fd5f166187c2
Nicolas Alfonso <nicolas7314@outlook.com>
2025-04-07 08:15:35 +0100

    Update ftstrnstr.c

--- ftstrnstr.c
+++ ftstrnstr.c
@@ -1,4 +1,12 @@
-char *ft_strnstr(char *str, char find_str)
+char *ft_strnstr(char *str, char find_str, int n)
 {
+    int i;
+
+    i = 0;
+    while(i < n)
+    {
+        if (str[i] )
+    }
+    
     
 }
